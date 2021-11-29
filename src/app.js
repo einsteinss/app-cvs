1f85f0b5e6d6406f19924a8ef478b12d9bbdf4f7
Pedro Kussy <pedrokussy@gmail.com>
2021-11-29 22:05:44 +0100

    configurei a estrutura básica do express no app.ja

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
 require("dotenv").config();
+
+const express = require("express");
+const app = express();
+app.set("port", 3081 || process.env.PORT)
+
+
+
+app.listen(app.get("port"), function(){
+	console.log("Server running!")
+})
