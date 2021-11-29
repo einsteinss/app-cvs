69c0a6726949d183163a99bc84af0d2cd9f7c161
Pedro Kussy <pedrokussy@gmail.com>
2021-11-29 23:00:33 +0100

    Criei a o modulo de conexão com o MySQL . Usei async await nas função de conexão e usei também closure de modo que, assim que a funcão é chamada, ela já é executada

---
+++ src/dbconfig/mysql.config.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2');
+
+const connection = (async () => {
+    await const con =   mysql.createConnection({
+        host:process.env.MYSQL_DB_HOST,
+        user:process.env.MYSQL_DB_USER,
+        password:process.env.MYSQL_DB_PASS,
+        database:process.env.MYSQL_DB_NAME,
+        port:process.env.MYSQL_DB_PORT
+    });
+
+    await con.connect((err) => {
+        if (err) {
+            console.log('Erro ao conectar com a base de dados:', err)
+            return
+        }
+        console.log('Conexão estabelecida com a base de dados')
+    })
+})()
+
+module.exports = connection
