0e75f4d4bc3122a58ac337ca784bf4fce58daebc
Pedro Kussy <pedrokussy@gmail.com>
2021-12-15 00:54:52 +0100

    instalei modulos e configurei algumas coisas...

--- src/app.js
+++ src/app.js
@@ -2,13 +2,16 @@ require("dotenv").config();
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mogran = require("morgan")
 const app = express();
 
 app.set("port",process.env.PORT);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use("/", require("./route/routes");
+app.use(mogran("dev"))
+
+app.use("/", require("./route/routes"))
 
 app.listen(app.get("port"), function(){
 	console.log("Server running!");
