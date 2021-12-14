42344ebfab435ad28ae1cdd3143bc270d3fe2ad1
Pedro Kussy <pedrokussy@gmail.com>
2021-12-15 00:54:00 +0100

    conexao com a MongoDB - cloud, estabelecida

--- src/infra/mongo-database.js
+++ src/infra/mongo-database.js
@@ -1,2 +1,10 @@
-const mongod = require("mongodb")
-
+require("dotenv").config()
+const mongoose = require("mongoose")
+mongoose.Promise = global.Promise
+mongoose.connect("mongodb+srv://cvs_UseR:Q3VBFzUP5WBSmmA@cvsdb.t9arx.mongodb.net/cvsdb?retryWrites=true&w=majoritys")
+	.then(()=>{
+		console.log("MongoDb connected")
+	}).catch(err =>{
+		console.log("Erro: "+err)
+	})
+module.exports = mongoose
